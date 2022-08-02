6708ba187e46b18f5109dcbc0a92f23252a265c0
Galo98 <galonahuel98@gmail.com>
2022-08-02 19:55:37 -0300

    Inicializacion del proyecto

---
+++ js/agregar-personas.js
@@ -0,0 +1,44 @@
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+
+botonAdicionar.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicionar");
+
+    var tabla = document.querySelector("#tabla-pacientes");
+
+    var nombre = form.nombre.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    //Crear etiquetas 1 tr y 5 td
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add("paciente");
+    var nombreTd = document.createElement("td");
+    nombreTd.classList.add("info-nombre");
+    var pesoTd = document.createElement("td");
+    pesoTd.classList.add("info-peso");
+    var alturaTd = document.createElement("td");
+    alturaTd.classList.add("info-altura");
+    var gorduraTd = document.createElement("td");
+    gorduraTd.classList.add("info-gordura");
+    var imcTd = document.createElement("td");
+    imcTd.classList.add("info-imc");
+
+    nombreTd.textContent = nombre;
+    alturaTd.textContent = altura;
+    pesoTd.textContent = peso;
+    gorduraTd.textContent = gordura;
+    imcTd.textContent = 0;
+
+    tabla.appendChild(pacienteTr);
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+    pacienteTr.appendChild(imcTd);
+
+    console.log(pacienteTr);
+
+});
---
+++ js/calcular-imc.js
@@ -0,0 +1,34 @@
+var erroresFondo = "red";
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++){
+    
+    var paciente = pacientes[i];
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    var tdIMC = paciente.querySelector(".info-imc");
+    var pesoV = true;
+    var alturaV = true;
+
+    if (peso < 0 || peso > 1000) {
+        tdIMC.textContent= "Peso Incorrecto";
+        console.log("Peso Incorrecto");
+        pesoV = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if (altura < 0 || altura >= 3.00) {
+        tdIMC.textContent= "Altura Incorrecta";
+        console.log("Altura Incorrecta");
+        alturaV = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+
+    if (pesoV && alturaV) {
+
+        var imc = peso / (altura * altura);
+        tdIMC.textContent = imc.toFixed(2);
+    }   
+}
